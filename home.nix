19a8de9da700211302eacade0bf2e6aff5e7d945
Luke Rewega <lrewega@c32.ca>
2024-02-07 19:11:50 -0500

    Just own the LSPs; molokai

--- home.nix
+++ home.nix
@@ -51,13 +51,15 @@
       enableBashIntegration = true;
     };
 
-    git = let
+    git =
+      let
         gitBufUserAndEmailConfig = pkgs.writeText "buf_user_and_email.inc" ''
           [user]
             name = Luke Rewega
             email = lrewega@buf.build
         '';
-      in {
+      in
+      {
         enable = true;
         package = pkgs.gitAndTools.gitFull;
         extraConfig = {
@@ -92,203 +94,246 @@
       '';
     };
 
-    vim = {
-      enable = true;
 
-      plugins = let
-        vim-synthwave84 = pkgs.vimUtils.buildVimPlugin {
-          name = "";
-          src = pkgs.fetchFromGitHub {
-            owner = "artanikin";
-            repo = "vim-synthwave84";
-            rev = "a5caa80d9e1a7021f9ec6c06a96d09dfc2d99ed1";
-            hash = "sha256-5+rOp2xDdtIMxMdvV0N18yTSQuSzYIfnFvwNeufaDgQ=";
+    vim =
+      let
+        lsp = {
+          yaml-language-server = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+          json-language-server = "${pkgs.vscode-langservers-extracted}/bin/vscode-json-language-server";
+          nix-language-server = "${pkgs.nil}/bin/nil";
+        };
+        nixpkgs-fmt = "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+        extraPlugins = {
+          vim-synthwave84 = pkgs.vimUtils.buildVimPlugin {
+            name = "vim-synthwave84";
+            src = pkgs.fetchFromGitHub {
+              owner = "artanikin";
+              repo = "vim-synthwave84";
+              rev = "a5caa80d9e1a7021f9ec6c06a96d09dfc2d99ed1";
+              hash = "sha256-5+rOp2xDdtIMxMdvV0N18yTSQuSzYIfnFvwNeufaDgQ=";
+            };
+          };
+          badwolf = pkgs.vimUtils.buildVimPlugin {
+            name = "badwolf";
+            src = pkgs.fetchFromGitHub {
+              owner = "sjl";
+              repo = "badwolf";
+              rev = "3fae5a19ed14b37f025abc366f2e738046b7cb42";
+              hash = "sha256-RWQfEXSrKRTMTPw17du0GfRDTafDBtuug4hwWUc6vcg=";
+            };
+          };
+          molokayo = pkgs.vimUtils.buildVimPlugin {
+            name = "molokayo";
+            src = pkgs.fetchFromGitHub {
+              owner = "fmoralesc";
+              repo = "molokayo";
+              rev = "d45f3acaa75559f5ad04f74aec90c73295321074";
+              hash = "sha256-RWQfEXSrKRTMTPw17du0GfRDTafDBtuug4hwWUc6vcg=";
+            };
           };
         };
-      in with pkgs.vimPlugins; [
-        # Languages
-        mkdx
-        vim-go
-        vim-nix
-        # Themes
-        onedark-vim
-        papercolor-theme
-        # Misc.
-        nerdtree
-        vim-commentary
-        vim-fugitive
-        vim-gitgutter
-        vim-helm
-        vim-lsp
-        vim-sensible
-        vim-sleuth
-      ] ++ [
-        vim-synthwave84
-      ];
+      in
+      {
+        enable = true;
+        # Full list of available plugins:
+        # https://github.com/NixOS/nixpkgs/blob/nixpkgs-unstable/pkgs/applications/editors/vim/plugins/vim-plugin-names
+        plugins =
+          builtins.attrValues
+            {
+              inherit (pkgs.vimPlugins)
+                # Languages
+                mkdx
+                vim-go
+                vim-nix
+                # Themes
+                gruvbox
+                molokai
+                jellybeans-vim
+                onedark-vim
+                papercolor-theme
+                # Misc.
+                nerdtree
+                vim-commentary
+                vim-fugitive
+                vim-gitgutter
+                vim-helm
+                vim-lsp
+                vim-sensible
+                vim-sleuth
+                ;
+            } ++ builtins.attrValues extraPlugins;
 
-      settings = {
-        relativenumber = true;
-      };
+        settings = {
+          relativenumber = true;
+        };
 
-      extraConfig = ''
-        set hlsearch
-        set textwidth=100
-        if has('termguicolors')
-          set termguicolors
-        endif
-        set background=dark
+        extraConfig = ''
+          set hlsearch
+          set textwidth=100
+          if has('termguicolors')
+            set termguicolors
+          endif
+          set background=dark
 
-        colorscheme PaperColor
+          colorscheme PaperColor
 
-        " Enable extra highlighting features for Go (vim-go)
-        let g:go_highlight_trailing_whitespace_error = 1
-        let g:go_highlight_functions = 1
-        let g:go_highlight_function_parameters = 1
-        let g:go_highlight_function_calls = 1
-        let g:go_highlight_types = 1
-        let g:go_highlight_fields = 1
-        let g:go_highlight_build_constraints = 1
-        let g:go_highlight_generate_tags = 1
-        let g:go_highlight_string_spellcheck = 1
-        let g:go_highlight_format_strings = 1
-        let g:go_highlight_variable_declarations = 1
-        let g:go_highlight_variable_assignments = 1
+          " Enable extra highlighting features for Go (vim-go)
+          let g:go_highlight_trailing_whitespace_error = 1
+          let g:go_highlight_functions = 1
+          let g:go_highlight_function_parameters = 1
+          let g:go_highlight_function_calls = 1
+          let g:go_highlight_types = 1
+          let g:go_highlight_fields = 1
+          let g:go_highlight_build_constraints = 1
+          let g:go_highlight_generate_tags = 1
+          let g:go_highlight_string_spellcheck = 1
+          let g:go_highlight_format_strings = 1
+          let g:go_highlight_variable_declarations = 1
+          let g:go_highlight_variable_assignments = 1
 
-        " More Go knobs
-        let g:go_doc_popup_window = 1
-        let g:go_doc_balloon = 1
-        autocmd BufNewFile,BufRead *.go setlocal mouse=a ttymouse=sgr balloonexpr=go#tool#DescribeBalloon() balloondelay=250 balloonevalterm
+          " More Go knobs
+          let g:go_doc_popup_window = 1
+          let g:go_doc_balloon = 1
+          autocmd BufNewFile,BufRead *.go setlocal mouse=a ttymouse=sgr balloonexpr=go#tool#DescribeBalloon() balloondelay=250 balloonevalterm
 
-        " Reapply syntax highlight groups when changing colorscheme
-        augroup ColourSchemeReload
-            au!
-            au ColorScheme * call <SID>FixSyntaxHighlightGroups()
-            au ColorScheme PaperColor highlight SignColumn guibg=Black " Fix PaperColor
-        augroup END
+          " Reapply syntax highlight groups when changing colorscheme
+          augroup ColourSchemeReload
+              au!
+              au ColorScheme * call <SID>FixSyntaxHighlightGroups()
+              au ColorScheme PaperColor highlight SignColumn guibg=Black " Fix PaperColor
+          augroup END
 
-        function s:FixSyntaxHighlightGroups()
-            let b = bufnr()
-            let &l:filetype = &l:filetype
-            bufdo let &l:filetype = &l:filetype
-            exec "buffer" b
-        endfunction
+          function s:FixSyntaxHighlightGroups()
+              let b = bufnr()
+              let &l:filetype = &l:filetype
+              bufdo let &l:filetype = &l:filetype
+              exec "buffer" b
+          endfunction
 
-        " PaperColor
-        let g:PaperColor_Theme_Options = {
-          \   'theme': {
-          \     'default': {
-          \       'transparent_background': 1
-          \     }
-          \   }
+          " PaperColor
+          let g:PaperColor_Theme_Options = {
+            \   'theme': {
+            \     'default': {
+            \       'transparent_background': 1
+            \     }
+            \   }
+            \ }
+
+          " mkdx
+          let g:mkdx#settings = {
+          \   'map': { 'prefix': '<Space>' }
           \ }
 
-        " mkdx
-        let g:mkdx#settings = {
-        \ 'map': { 'prefix': '<Space>' }
-        \ }
-
-        " LSP for *.nix
-        if executable('nil')
-          autocmd User lsp_setup call lsp#register_server({
-            \   'name': 'nil',
-            \   'cmd': {server_info->[&shell, &shellcmdflag, 'nil']},
+          " LSP for *.nix
+          let s:lsp_exe_nix = '${lsp.nix-language-server}'
+          let s:nixpkgs_fmt = '${nixpkgs-fmt}'
+          if executable(s:lsp_exe_nix)
+            autocmd User lsp_setup call lsp#register_server({
+              \   'name': 'nil',
+              \   'cmd': {server_info->[&shell, &shellcmdflag, s:lsp_exe_nix]},
+              \   'allowlist': ['nix'],
+              \ })
+            if executable(s:nixpkgs_fmt)
+              autocmd User lsp_setup call lsp#update_workspace_config('nil', {
+              \   'nil': {
+              \     'formatting': {
+              \       'command': [s:nixpkgs_fmt],
+              \     },
+              \   },
+              \ })
+              autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.nix')
+            endif
+          elseif executable('rnix-lsp')
+            autocmd User lsp_setup call lsp#register_server({
+            \   'name': 'rnix-lsp',
+            \   'cmd': {server_info->[&shell, &shellcmdflag, 'rnix-lsp']},
             \   'allowlist': ['nix'],
-            \ })
-          if executable('nixpkgs-fmt')
-            autocmd User lsp_setup call lsp#update_workspace_config('nil', {
-            \   'nil': {
-            \     'formatting': {
-            \       'command': ['nixpkgs-fmt'],
-            \     },
-            \   },
             \ })
             autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.nix')
           endif
-        elseif executable('rnix-lsp')
-          autocmd User lsp_setup call lsp#register_server({
-          \   'name': 'rnix-lsp',
-          \   'cmd': {server_info->[&shell, &shellcmdflag, 'rnix-lsp']},
-          \   'allowlist': ['nix'],
-          \ })
-          autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.nix')
-        endif
 
-        " LSP for *.yaml
-        if executable('yaml-language-server')
-          autocmd User lsp_setup call lsp#register_server({
-          \   'name': 'yaml-language-server',
-          \   'cmd': {server_info->[&shell, &shellcmdflag, 'yaml-language-server --stdio']},
-          \   'allowlist': ['yaml'],
-          \   'root_uri': {-> s:root_uri('.git')},
-          \   'workspace_config': {
-          \     'yaml': {
-          \       'format': {
-          \         'enable': v:true,
-          \       },
-          \       'schemas': {},
-          \       'schemaStore': {
-          \         'enable': v:true,
-          \         'url': "",
-          \       },
-          \     },
-          \   },
-          \ })
-          autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.yaml')
-        endif
+          " LSP for *.yaml
+          let s:lsp_exe_yaml = '${lsp.yaml-language-server}'
+          if executable(s:lsp_exe_yaml)
+            autocmd User lsp_setup call lsp#register_server({
+            \   'name': 'yaml-language-server',
+            \   'cmd': {server_info->[&shell, &shellcmdflag, s:lsp_exe_yaml . ' --stdio']},
+            \   'allowlist': ['yaml'],
+            \   'root_uri': {-> s:root_uri('.git')},
+            \   'workspace_config': {
+            \     'yaml': {
+            \       'completion': v:true,
+            \       'format': {
+            \         'enable': v:true,
+            \       },
+            \       'hover': v:true,
+            \       'schemas': {},
+            \       'schemaStore': {
+            \         'enable': v:true,
+            \         'url': "",
+            \       },
+            \       'validate': v:true,
+            \     },
+            \   },
+            \ })
+            autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.yaml')
+          endif
 
-        " LSP for *.json
-        if executable('vscode-json-language-server')
-          autocmd User lsp_setup call lsp#register_server({
-          \   'name': 'vscode-json-language-server',
-          \   'cmd': {server_info->[&shell, &shellcmdflags, 'vscode-json-language-server --stdio']},
-          \   'allowlist': ['json'],
-          \   'root_uri': {-> s:root_uri('.git')},
-          \   'workspace_config': {
-          \     'json': {
-          \       'format': {
-          \         'enable': v:true,
-          \       },
-          \       'schemas': {},
-          \       'schemaStore': {
-          \         'enable': v:true,
-          \       },
-          \     },
-          \   },
-          \ })
-          autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.json')
-        endif
+          " LSP for *.json
+          let s:lsp_exe_json = '${lsp.json-language-server}'
+          if executable(s:lsp_exe_json)
+            autocmd User lsp_setup call lsp#register_server({
+            \   'name': 'vscode-json-language-server',
+            \   'cmd': {server_info->[&shell, &shellcmdflags, s:lsp_exe_json . ' --stdio']},
+            \   'allowlist': ['json'],
+            \   'root_uri': {-> s:root_uri('.git')},
+            \   'workspace_config': {
+            \     'json': {
+            \       'format': {
+            \         'enable': v:true,
+            \       },
+            \       'schemas': {},
+            \       'schemaStore': {
+            \         'enable': v:true,
+            \       },
+            \     },
+            \   },
+            \ })
+            autocmd User lsp_buffer_enabled call s:on_lsp_buffer_enabled('*.json')
+          endif
 
-        " LSP helper function to find a reasonable filesystem root path for a project.
-        function s:root_uri(...) abort
-          function s:join(...) abort
-            let sep = has('win32') ? '\' : '/'
-            return join(map(copy(a:000), "substitute(v:val, '[/\\\\]\\+$', \"\", \"\")"), sep)
+          " LSP helper function to find a reasonable filesystem root path for a project.
+          function s:root_uri(...) abort
+            function s:join(...) abort
+              let sep = has('win32') ? '\' : '/'
+              return join(map(copy(a:000), "substitute(v:val, '[/\\\\]\\+$', \"\", \"\")"), sep)
+            endfunction
+            let root = expand('%:p:h')
+            let prev = ""
+            while root !=# prev
+              for name in a:000
+                if getftype(s:join(root, name)) !=# ""
+                  return lsp#utils#path_to_uri(root)
+                endif
+              endfor
+              let prev = root
+              let root = fnamemodify(root, ':h')
+            endwhile
+            return lsp#utils#get_default_root_uri()
           endfunction
-          let root = expand('%:p:h')
-          let prev = ""
-          while root !=# prev
-            for name in a:000
-              if getftype(s:join(root, name)) !=# ""
-                return lsp#utils#path_to_uri(root)
-              endif
-            endfor
-            let prev = root
-            let root = fnamemodify(root, ':h')
-          endwhile
-          return lsp#utils#get_default_root_uri()
-        endfunction
 
-        " Stuff to run when LSP is engaged for a buffer
-        function s:on_lsp_buffer_enabled(matches)
-          setlocal omnifunc=lsp#complete
-          setlocal signcolumn=yes
-          nmap <buffer> K <plug>(lsp-hover)
-          let g:lsp_format_sync_timeout = 1000
-          autocmd BufWritePre a:matches call execute('LspDocumentFormatSync')
-        endfunction
-      '';
+          " Stuff to run when LSP is engaged for a buffer
+          function s:on_lsp_buffer_enabled(matches)
+            setlocal updatetime=250
+            setlocal omnifunc=lsp#complete
+            setlocal signcolumn=yes
+            nmap <buffer> K <plug>(lsp-hover)
+            let g:lsp_format_sync_timeout = 1000
+            autocmd BufWritePre a:matches call execute('LspDocumentFormatSync')
+          endfunction
+        '';
 
-    };
+      };
   };
 
   home.sessionVariables.EDITOR = "vim";
